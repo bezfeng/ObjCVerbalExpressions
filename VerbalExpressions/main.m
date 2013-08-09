003a049ae6ac1f35ce206e25766e9194a9d99648
Bezhou Feng <Bezhou_Feng@comcast.com>
2013-08-09 15:40:11 -0400

    Initial Commit

---
+++ VerbalExpressions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  VerbalExpressions
+//
+//  Created by Feng, Bezhou on 8/9/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
